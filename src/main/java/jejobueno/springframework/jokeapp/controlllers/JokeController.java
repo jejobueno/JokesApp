da383bafe6719192fd58b8451623ad2f042f7d5b
Jesus Bueno <jessbuenos@gmail.com>
2021-01-29 07:18:28 +0100

    Map context root done. Joke text added, view name "chucknorris" added. Closes #1,#2,#3

---
+++ src/main/java/jejobueno/springframework/jokeapp/controlllers/JokeController.java
@@ -0,0 +1,27 @@
+package jejobueno.springframework.jokeapp.controlllers;
+
+import jejobueno.springframework.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public  JokeController(JokeService jokeService){
+        this.jokeService = jokeService;
+    }
+
+    //It will take in an model and we are adding an attribute that
+    // are the random jokes. We also are giving name view "chucknorris"
+    @RequestMapping({"/",""}) //Map context root
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
